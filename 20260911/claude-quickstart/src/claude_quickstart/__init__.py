f11179a4aa5a314d2395772803d337bb399db406
Orvin Jed Alava <orvinjedalava@Orvins-Mac-mini.local>
2026-09-11 18:09:11 +1000

    My first LLM call

--- 20260911/claude-quickstart/src/claude_quickstart/__init__.py
+++ 20260911/claude-quickstart/src/claude_quickstart/__init__.py
@@ -1,2 +1,24 @@
+import anthropic
+from anthropic.types import Message, ContentBlock
+
+client = anthropic.Anthropic()
+
+# claude-haiku-4-5-20251001
+
 def main() -> None:
-    print("Hello from claude-quickstart!")
+    print("Making a call to the LLM...")
+
+    message: Message = client.messages.create(
+        model="claude-haiku-4-5-20251001",
+        max_tokens=1000,
+        messages=[
+            {
+                "role": "user",
+                "content": "What should I search for to find the latest developments in renewable energy?",
+            }
+        ],
+    )
+
+    for block in message.content:
+        if block.type == "text":
+            print(block.text)
